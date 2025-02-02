f4b84650c0a3f6edcc72b53fde30da81eb6ae483
dago-developer <contact.dago.dev@gmail.com>
2025-02-01 22:02:16 -0400

    fixing errores !!!

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,24 +1,53 @@
+
+const membersContainer = document.getElementById('members-container');
+const toggleViewBtn = document.getElementById('toggleViewBtn');
+
+let isGridView = true;
+
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    const header = document.querySelector('.header');
+    header.classList.toggle('open');
+});
+
+
+function createMemberCards(members) {
+    membersContainer.innerHTML = '';  
+    members.forEach(member => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="images/${member.image}" alt="${member.name}" class="card-image">
+            <h3 class="card-name">${member.name}</h3>
+            <p class="card-address">${member.address}</p>
+            <p class="card-phone">${member.phone}</p>
+            <a href="${member.website}" target="_blank" class="card-website">Visit Website</a>
+        `;
+        
+        membersContainer.appendChild(card);
+    });
+}
+
+function toggleView() {
+    if (isGridView) {
+        membersContainer.classList.remove('grid-view');
+        membersContainer.classList.add('list-view');
+        toggleViewBtn.textContent = 'Switch to Grid View'; 
+    } else {
+        membersContainer.classList.remove('list-view');
+        membersContainer.classList.add('grid-view');
+        toggleViewBtn.textContent = 'Switch to List View';  
+    }
+    isGridView = !isGridView;  
+}
+
 fetch('data/members.json')
-    .then(response => response.json())  
+    .then(response => response.json())
     .then(members => {
-        const directoryContainer = document.getElementById('directory');
-
-        members.forEach(member => {
-            const card = document.createElement('div');
-            card.classList.add('card');
-
-            card.innerHTML = `
-                <img src="images/${member.image}" alt="${member.name}" class="card-image">
-                <h2 class="card-name">${member.name}</h2>
-                <p class="card-address">${member.address}</p>
-                <p class="card-phone">${member.phone}</p>
-                <a href="${member.website}" class="card-website" target="_blank">Visitar Sitio Web</a>
-                <p class="card-membership">Membresía: ${member.membership}</p>
-            `;
-
-            directoryContainer.appendChild(card);
-        });
+        createMemberCards(members);
     })
     .catch(error => {
-        console.error('Error al cargar el archivo JSON:', error);
+        console.error('Error al cargar los miembros:', error);
     });
+
+toggleViewBtn.addEventListener('click', toggleView);
